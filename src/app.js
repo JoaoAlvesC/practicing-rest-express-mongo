f0d12ea7c6046b7a4266cba3283fd27f4a0e3e60
JoaoAlvesC <jooao.v@outlook.com>
2022-03-23 21:14:11 -0300

    refactor: finish CRUD and structure separation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import db from "./config/dbConnect.js"
 import express from "express"
-import livros from "./models/Livro.js"
 import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, "Erro de conexão"))
@@ -10,26 +9,6 @@ db.once("open", () => {
 
 const app = express();
 app.use(express.json())
-
 routes(app)
-
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id)
-    res.json(livros[index])
-})
-    
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id)
-    livros[index].titulo = req.body.titulo
-    res.json(livros)
-})
-
-app.delete('/livros/:id', (req, res) => {
-    let {id} = req.params
-    let index = buscaLivro(id)
-    livros.splice(index, 1)
-    res.send(`Livro ${id} removido com sucesso!`)
-})
-
 
 export default app;
--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -8,16 +8,53 @@ class LivroController {
         })
     }
 
+    static listarLivroPorId = (req, res) => {
+        const id = req.params.id;
+
+        livros.findById(id, (err, livro) => {
+            if(!err){
+                res.status(200).send(livro)
+            }else{
+                res.status(400).send({message: err.message})
+            }
+        })
+    }
+
     static cadastrarLivro = (req, res) => {
         let livro = new livros(req.body);
         livro.save((err) => {
             if(err){
-                res.status(500).send({message: `${err.message} - falha ao cadastrar livro.`})
+                res.status(500).send({message: `${err.message} - falha ao cadastrar livro.`});
             }else{
-                res.status(201).send(livro.toJSON())
+                res.status(201).send(livro.toJSON());
             }
         })
     }
+
+    static atualizarLivro = (req, res) => {
+        const id = req.params.id;
+
+        livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(!err){
+                res.status(200).send({message: "Livro atualizado com sucesso"});
+            }else{
+                res.status(500).send({message: err.message})
+            }
+        })
+    }
+
+    static excluirLivro = (req, res) => {
+        const id = req.params.id;
+
+        livros.findByIdAndDelete(id, (err) => {
+            if(!err){
+                res.status(200).send({message: "Livro removido com sucesso"});
+            }else{
+                res.status(500).send({message: err.message})
+            }
+        })
+    }
+
 
 }
 
